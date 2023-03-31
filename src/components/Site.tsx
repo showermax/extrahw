c831bc4621e8671d73a12d9cbe79098f03aa5583
showermax <sevenhauz@gmail.com>
2023-03-31 18:17:30 +0300

    hw2 routes

---
+++ src/components/Site.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './Site.module.css'
+import {Routes, Route, Navigate} from "react-router-dom";
+import {PageOne} from "./pages/PageOne";
+import {PageTwo} from "./pages/PageTwo";
+import {PageThree} from "./pages/PageThree";
+
+
+export const Site = () => {
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    1234
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path={'/'} element={<Navigate to={'/page1'} />} />
+                        <Route path={'/page1'} element={<PageOne/>}/>
+                        <Route path={'/page2'} element={<PageTwo/>}/>
+                        <Route path={'/page3'} element={<PageThree/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    );
+};
+
