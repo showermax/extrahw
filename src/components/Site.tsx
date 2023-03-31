0cfa1debdcc31eec2faddc74ee7c8f3fad843370
showermax <sevenhauz@gmail.com>
2023-03-31 18:25:03 +0300

    hw2 routes navlinks

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './Site.module.css'
-import {Routes, Route, Navigate} from "react-router-dom";
+import {Routes, Route, Navigate, NavLink} from "react-router-dom";
 import {PageOne} from "./pages/PageOne";
 import {PageTwo} from "./pages/PageTwo";
 import {PageThree} from "./pages/PageThree";
+import {Error404} from "./pages/Error404";
 
 
 export const Site = () => {
@@ -12,14 +13,17 @@ export const Site = () => {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    1234
+                    <div><NavLink to={'/page 1'}>Hello page 1  </NavLink></div>
+                    <div><NavLink to={'/page 2'}>Hello page 2  </NavLink></div>
+                    <div><NavLink to={'/page 3'}>Hello page 3  </NavLink></div>
                 </div>
                 <div className={styles.content}>
                     <Routes>
                         <Route path={'/'} element={<Navigate to={'/page1'} />} />
-                        <Route path={'/page1'} element={<PageOne/>}/>
-                        <Route path={'/page2'} element={<PageTwo/>}/>
-                        <Route path={'/page3'} element={<PageThree/>}/>
+                        <Route path={'/page1'} element={<PageOne />} />
+                        <Route path={'/page2'} element={<PageTwo />} />
+                        <Route path={'/page3'} element={<PageThree />} />
+                        <Route path={'/*'} element={<Error404 />} />
                     </Routes>
                 </div>
             </div>
