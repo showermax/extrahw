92ed2ccc679693e262ec22f67eca1f5b42518db8
showermax <sevenhauz@gmail.com>
2023-04-07 18:35:37 +0300

    extra 0704 double id useparams

--- src/ExtraHW0704/components/pages/Page.tsx
+++ src/ExtraHW0704/components/pages/Page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {PagesType} from "../../dataState/dataState";
 import {useParams} from "react-router-dom";
 import {Content} from "./Content";
+import {Error404} from "./Error404";
 
 type PagePropsType = {
     pages: Array<PagesType>
@@ -13,7 +14,9 @@ export const Page = (props: PagePropsType) => {
   //  console.log(param) //{id: "0", newID:"00"}
 
     return (
+       Number(param.id)<props.pages.length-1 ?
         <Content heading={props.pages[Number(param.id)].heading} pages={props.pages[Number(param.id)].about}/>
+           : <Error404/>
         // <div>
         //     <div>
         //         {props.pages[Number(param.id)].heading}
